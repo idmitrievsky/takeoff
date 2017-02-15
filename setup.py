58b5c6f8ea3e30b83fbec7e037c7a3ed56c21a2c
Ivan Dmitrievsky <ivan.dmitrievsky@gmail.com>
2017-02-15 13:56:02 +0300

    Fix style checks

--- setup.py
+++ setup.py
@@ -2,24 +2,23 @@
 
 from setuptools import setup, find_packages
 
-
 with open('README.rst') as f:
     readme = f.read()
 
 with open('LICENSE') as f:
-    license = f.read()
+    license_ = f.read()
 
 setup(
-    name='takeoff', # FIXME
+    name='takeoff',  # FIXME
     version='0.1.0',
-    description='An easy to use project template for Python', # FIXME
+    description='An easy to use project template for Python',  # FIXME
     long_description=readme,
-    author='Ivan Dmitrievsky', # FIXME
-    author_email='ivan.dmitrievsky+python@gmail.com', # FIXME
-    url='https://github.com/idmit/takeoff', # FIXME
+    author='Ivan Dmitrievsky',  # FIXME
+    author_email='ivan.dmitrievsky+python@gmail.com',  # FIXME
+    url='https://github.com/idmit/takeoff',  # FIXME
     install_requires=[
         # FIXME
     ],
-    license=license,
+    license=license_,
     packages=find_packages(exclude=('tests', 'docs'))
 )
--- tests/test_core.py
+++ tests/test_core.py
@@ -4,5 +4,6 @@ import pytest
 
 from takeoff.core import takeoff
 
+
 def test_takeoff():
     assert takeoff() == 'Takeoff complete!'
